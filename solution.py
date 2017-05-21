eb695aa9a302d97865bc4d441818db404fa9706e
vincentmhhon <vincentmhhon@gmail.com>
2017-05-21 20:07:32 +0800

    misc updates

--- solution.py
+++ solution.py
@@ -28,6 +28,27 @@ def naked_twins(values):
 
     # Find all instances of naked twins
     # Eliminate the naked twins as possibilities for their peers
+    two_elements_boxes = [box for box in values.keys() if len(values[box]) == 2]
+    """
+    for box in two_elements_boxes:
+        two_elements_peers = [x for x in two_elements_boxes if x in peers[box]]
+        print(two_elements_peers)
+        twin = ""
+        for peer in two_elements_peers:
+            print(values[peer])
+            if twin == values[peer]:
+                print("twin " + twin)
+                for digit in twin:
+                    for p in peers[box]:
+                        print("DDDD " + digit)
+                        print(values[p])
+                        values[p] = values[p].replace(digit, "")
+                        print(p + "  " + values[p])
+            else:
+                twin = values[peer]
+        print("***************")
+    """
+    return values
 
 
 def cross(a, b):
@@ -49,7 +70,7 @@ diagonal_units = [diagonal(rows, cols)]
 r_diagonal_units = [diagonal(rows, cols[::-1])]
 square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')]
 
-unit_list = row_units + column_units + square_units
+unit_list = row_units + column_units + square_units + diagonal_units + r_diagonal_units
 units = dict((s, [u for u in unit_list if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s], []))-set([s])) for s in boxes)
 
@@ -91,7 +112,7 @@ def display(values):
 
 
 def eliminate(values):
-    sin_boxes = [ box for box in values.keys() if len(values[box]) == 1 ]
+    sin_boxes = [box for box in values.keys() if len(values[box]) == 1 ]
     for box in sin_boxes:
             for peer in peers[box]:
                 values[peer] = values[peer].replace(values[box], "")
@@ -113,9 +134,11 @@ def reduce_puzzle(values):
         # Check how many boxes have a determined value
         solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
 
-        # Your code here: Use the Eliminate Strategy
+        # Use the Eliminate Strategy
         values = eliminate(values)
-        # Your code here: Use the Only Choice Strategy
+        # Use the Naked Twins Strategy
+        values = naked_twins(values)
+        # Use the Only Choice Strategy
         values = only_choice(values)
         # Check how many boxes have a determined value, to compare
         solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
@@ -157,13 +180,26 @@ def solve(grid):
     Returns:
         The dictionary representation of the final sudoku grid. False if no solution exists.
     """
-    return grid_values(grid)
+    return search(grid_values(grid))
 
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
-    s_diagonal_units = diagonal(rows, cols[::-1])
-    print(s_diagonal_units)
-    #display(solve(diag_sudoku_grid))
+    #print("Before solved")
+    #display(dict(zip(boxes, diag_sudoku_grid)))
+    #print("After solved")
+   # display(solve(diag_sudoku_grid))
+    a = {'I6': '4', 'H9': '3', 'I2': '6', 'E8': '1', 'H3': '5', 'H7': '8', 'I7': '1', 'I4': '8',
+     'H5': '6', 'F9': '7', 'G7': '6', 'G6': '3', 'G5': '2', 'E1': '8', 'G3': '1', 'G2': '8',
+     'G1': '7', 'I1': '23', 'C8': '5', 'I3': '23', 'E5': '347', 'I5': '5', 'C9': '1', 'G9': '5',
+     'G8': '4', 'A1': '1', 'A3': '4', 'A2': '237', 'A5': '9', 'A4': '2357', 'A7': '27',
+     'A6': '257', 'C3': '8', 'C2': '237', 'C1': '23', 'E6': '579', 'C7': '9', 'C6': '6',
+     'C5': '37', 'C4': '4', 'I9': '9', 'D8': '8', 'I8': '7', 'E4': '6', 'D9': '6', 'H8': '2',
+     'F6': '125', 'A9': '8', 'G4': '9', 'A8': '6', 'E7': '345', 'E3': '379', 'F1': '6',
+     'F2': '4', 'F3': '23', 'F4': '1235', 'F5': '8', 'E2': '37', 'F7': '35', 'F8': '9',
+     'D2': '1', 'H1': '4', 'H6': '17', 'H2': '9', 'H4': '17', 'D3': '2379', 'B4': '27',
+     'B5': '1', 'B6': '8', 'B7': '27', 'E9': '2', 'B1': '9', 'B2': '5', 'B3': '6', 'D6': '279',
+     'D7': '34', 'D4': '237', 'D5': '347', 'B8': '3', 'B9': '4', 'D1': '5'}
+    naked_twins(a)
 
     try:
         from visualize import visualize_assignments
